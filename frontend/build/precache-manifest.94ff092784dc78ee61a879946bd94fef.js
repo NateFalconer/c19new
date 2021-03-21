e0f9780fc81299872852e184342ef365d1086003
Nate Falconer <natefalconer@protonmail.com>
2021-03-21 18:21:33 -0400

    new

---
+++ frontend/build/precache-manifest.94ff092784dc78ee61a879946bd94fef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42249d449592d87a1e53f9bc8f8e8e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c8e46c8ba3a62723758",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d472f14b0e709db8bdca",
+    "url": "/static/css/main.8a66876e.chunk.css"
+  },
+  {
+    "revision": "5c8e46c8ba3a62723758",
+    "url": "/static/js/2.40bfe8de.chunk.js"
+  },
+  {
+    "revision": "93025073dffd6188623ea242ff1636ca",
+    "url": "/static/js/2.40bfe8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d472f14b0e709db8bdca",
+    "url": "/static/js/main.e92f1433.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  }
+]);
